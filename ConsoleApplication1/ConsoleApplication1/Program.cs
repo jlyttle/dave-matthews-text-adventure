9598c512ae3c69663c823b89e169be1d922626bf
Arekisanda-san <awalexander@smu.edu>
2015-07-17 22:34:05 -0500

    Revised decision system

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static int[] decisionArray = new int[10];
+        
         static void Main(string[] args)
         {
 
@@ -16,12 +16,12 @@ namespace ConsoleApplication1
             Text("Directed by Yung Chang", false);
             Text("Music provided by Scott Oelkers and Hatsune Miku", false);
             Text("Yeast-based nourishment provided by Daichi Shijima", true);
-            Decisions("Do you like big macs?", "Yes, big macs are love, big macs are life", "No, they have too much cholesterol", 0);
-            if (decisionArray[0] == 1)
+            int decisionValue = MakeDecision("Do you like big macs?", "Yes, big macs are love, big macs are life", "No, they have too much cholesterol", 0);
+            if (decisionValue == 1)
             {
                 DecisionZero.accept();
             }
-            if (decisionArray[0] == 2)
+            if (decisionValue == 2)
             {
                 DecisionZero.reject();
             }
@@ -37,43 +37,54 @@ namespace ConsoleApplication1
                 System.ConsoleKeyInfo keyPress = System.Console.ReadKey();
             }
         }
-        static void Decisions(string text, string decisionOne, string decisionTwo, int decisionNumber)
+        static int MakeDecision(string text, string decisionOne, string decisionTwo, int decisionNumber)
         {
 
             ConsoleKeyInfo decision;
             System.Console.WriteLine(text);
             System.Console.WriteLine("[Choose your meal size, Dave]");
-            System.Console.WriteLine("1.  Kid's Size:  " + decisionOne);
+            System.Console.WriteLine("1. Happy Meal:  " + decisionOne);
             System.Console.WriteLine("2. Super Size:  " + decisionTwo);
-            decision = System.Console.ReadKey();
-            if (decision.Key == ConsoleKey.D1)
+           
+            while(true)
             {
-                decisionArray[decisionNumber] = 1;
-            }
-            if (decision.Key == ConsoleKey.D2)
-            {
-                decisionArray[decisionNumber] = 2;
+                decision = System.Console.ReadKey();
+                System.Console.Write("\r");
+                if (decision.Key == ConsoleKey.D1)
+                {
+                    return 1;
+                }
+                if (decision.Key == ConsoleKey.D2)
+                {
+                    return 2;
+                }
+                System.Console.WriteLine("Don't be a Daichster, press 1 or 2.");
             }
         }
-        static void Decisions(string text, string decisionOne, string decisionTwo, string decisionThree, int decisionNumber)
+        static int MakeDecision(string text, string decisionOne, string decisionTwo, string decisionThree, int decisionNumber)
         {
             ConsoleKeyInfo decision;
             System.Console.WriteLine(text);
             System.Console.WriteLine("[Choose your meal size, Dave]");
             System.Console.WriteLine("1.  Kid's Size:  " + decisionOne);
             System.Console.WriteLine("2. Super Size:  " + decisionTwo);
-            decision = System.Console.ReadKey();
-            if (decision.Key == ConsoleKey.D1)
+            while (true)
             {
-                decisionArray[decisionNumber] = 1;
-            }
-            if (decision.Key == ConsoleKey.D2)
-            {
-                decisionArray[decisionNumber] = 2;
-            }
-            if (decision.Key == ConsoleKey.D3)
-            {
-                decisionArray[decisionNumber] = 3;
+                decision = System.Console.ReadKey();
+                System.Console.Write("\r");
+                if (decision.Key == ConsoleKey.D1)
+                {
+                    return 1;
+                }
+                if (decision.Key == ConsoleKey.D2)
+                {
+                    return 2;
+                }
+                if (decision.Key == ConsoleKey.D3)
+                {
+                    return 3;
+                }
+                System.Console.WriteLine("Don't be a Daichster, press 1, 2 or 3.");
             }
         }
     }
