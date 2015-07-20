341de29a43ae4936a9566c9ba9aacde7e6086eae
Arekisanda-san <awalexander@smu.edu>
2015-07-20 12:40:51 -0500

    Beginning work on combat system

--- ConsoleApplication1/ConsoleApplication1/Combat.cs
+++ ConsoleApplication1/ConsoleApplication1/Combat.cs
@@ -8,9 +8,62 @@ namespace ConsoleApplication1
 {
     class Combat
     {
-        //Hearts
+        //HP/Hearts
         int doritos;
-        //Magic
+        //Cholesterol/Magic
         float mountainDew;
+        //burgerPower
+        //Affects the damage multiplier for physical attacks
+        float strength = 10;
+
+        //glucose intake
+        //Affects the damage multiplier for magical attacks
+        int intelligence = 10;
+
+        // body fat
+        //Affects overall HP/Hearts
+        int endurance = 10;
+
+        // greasiness
+        //Affects turn order, higher levels of speed can lead to multiple hits, mildly affects chance to dodge
+        int speed = 10;
+
+        //Happy Meal Toys
+        //Affects chance of a successful persuasion
+        int charisma = 10;
+
+        //Playground skills
+        //Affects chance of dodging attacks, critical hits, maybe allow for alternative paths/shortcuts?
+        int agility = 10;
+
+        //Desire for a big mac
+        //Reduces cholesterol/Mountain Dew consumption
+        int willpower = 10;
+        
+        // Favor of Ronald
+        //Mildly affect everything
+        float luck = 10;
+        
+
+        //The effect a weapon has on a physical attack
+        float weaponPhysStrength = 1;
+
+        //The effect a weapon has on a magical attack
+        int weaponMagStrength = 1;
+        //The multiplier for critical hits
+        float critMultiplier = 2;
+
+         void PhysAttack()
+        {
+            //If no critical hit
+           // float attack = 1 * strength * weaponPhysStrength * luckEffect;
+            //If critical hit
+          //  float critAttack = 1 * strength * weaponPhysStrength * luckEffect * critMultiplier;
+        }
+        void MagicAttack()
+        {
+            
+        }
+
     }
 }
