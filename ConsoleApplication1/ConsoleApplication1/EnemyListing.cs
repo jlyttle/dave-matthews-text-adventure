cbbd6337156c4844e1e0337223482cb2806980f3
Arekisanda-san <awalexander@smu.edu>
2015-08-01 19:00:41 -0500

    Still working on dictionary.

--- ConsoleApplication1/ConsoleApplication1/EnemyListing.cs
+++ ConsoleApplication1/ConsoleApplication1/EnemyListing.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    public class EnemyListing
+    public class Enemy
     {
-        public static void EnemyCall()
+        public Enemy(string enemyName, float enemyHealth, float enemyMagic, int enemyStrength, int enemyIntelligence, int enemyEndurance, int enemySpeed, int enemyCharisma, int enemyAgility, int enemyWillpower, int enemyLuck)
         {
 
         }
+
+        //Put in the name of an enemy, and call all of its values
+        public static void EnemyCall(string enemyName)
+        {
+            Dictionary<string, string> enemyList = new Dictionary<string, string>();
+            Dictionary<string, string>.ValueCollection valueColl = enemyList.Values;
+
+
+            //Enemy list
+            //The name you'll want to put in when accessing the dictionary is the one serving as the enemyName value.
+            Enemy shinji = new Enemy("Shinji", 1, 0, 1, 1, 1, 1, 0, 1, 1, 0);
+            enemyList.Add("Shinji", "Shinji");
+            //
+            
+
+
+            if (enemyList.ContainsKey(enemyName))
+            {
+
+            }
+        }
     }
-}
+    }
+
