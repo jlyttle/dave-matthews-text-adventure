027fe14d15fc2b8054ef512fa185ebdf313814d1
Jonathan Lyttle <Jonathan Lyttle>
2016-04-17 05:09:39 -0500

    Started Cursor system

---
+++ ConsoleApplication1/ConsoleApplication1/Cursor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DaveMatthewsTextAdventure
+{
+    class Cursor
+    {
+        public static void WaitForKey()
+        {
+           System.ConsoleKeyInfo keyPress = System.Console.ReadKey();
+        }
+        public static int MakeChoice(int numChoices)
+        {
+            //NOTE: THE CHOICES HAVE TO HAVE A FREE LINE ABOVE AND BELOW THEM IN ORDER TO WORK PROPERLY.
+            //ALSO, YOU NEED TO HAVE AT LEAST ONE FREE SPACE BEFORE EACH OF THE CHOICES OR ELSE
+            //THE FIRST CHARACTER WILL BE LOST. YES, THIS IS BAD PROGRAMMING, BUT IT'S NOT HIGH ON THE
+            //LIST OF PRIORITIES TO FIX, CONSIDERING THAT I'M THE ONLY ONE WRITING THIS.
+            Console.SetCursorPosition(0, Console.CursorTop - (numChoices + 1));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\r>");
+            Console.CursorVisible = false;
+            int position = 0;
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+
+            do
+            {
+                while (Console.KeyAvailable == false)
+                    Thread.Sleep(50); // Loop until input is entered.
+
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.UpArrow && position > 0)
+                {
+                    Console.Write("\r ");
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    //SFX.Play("Selection");
+                    position--;
+                    Console.Write("\r>");
+                }
+                else if (key.Key == ConsoleKey.DownArrow && position < (numChoices - 1))
+                {
+                    Console.Write("\r ");
+                    Console.SetCursorPosition(0, Console.CursorTop + 1);
+                  //  SFX.Play("Selection");
+                    position++;
+                    Console.Write("\r>");
+                }
+            } while (key.Key != ConsoleKey.Enter);
+            Console.CursorVisible = true;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            return position + 1;
+            //Console.WriteLine("\rNRNRNRNRNRNRNRNRNRNRNRNRNRNRNRNRNR");
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Menu.cs
+++ ConsoleApplication1/ConsoleApplication1/Menu.cs
@@ -9,46 +9,50 @@ namespace DaveMatthewsTextAdventure
 {
     class Menu
     {
+        //Variables
+        int maxLocations = Player.dave.GetLocationList().Count + 1;
+
+
+        //Functions
+        public static void GoToLocation(Location location) //probably shouldn't be static
+        {
+            // If your choice is 1 (the first choice)
+            if (location.GetRoom() == "Home")
+            {
+                RunHomeStory();
+            }
+            //check to see if your choice is the same list value as the Home location
+            if (Player.dave.m_location.GetRoom().IndexOf("Home") == 0)
+            {
+                Player.dave.m_location.SetRoom("Home");
+            }
+            if (Player.dave.m_location.GetRoom().IndexOf("McDonalds") == 0)
+            {
+                //go to mcdonalds
+            }
+            if (Player.dave.m_location.GetRoom().IndexOf("Daichi's House") == 0)
+            {
+                //go to daichi's house
+            }
+        }
+
+        //public static void UseItem()
+        //{
+        //  if()
+        //if(Player.dave.GetInventory().IndexOf() == 0)
+        //}
         public static void MenuBuilder()
         {
             int maxItems = 3;
             System.Console.WriteLine("What will Dave do?");
+            System.Console.WriteLine("------------------------");
             System.Console.WriteLine(" 1. Talk");
             System.Console.WriteLine(" 2. Items");
             System.Console.WriteLine(" 3. Locations");
+            System.Console.WriteLine("------------------------");
 
-            Console.SetCursorPosition(0, Console.CursorTop - (maxItems));
-            Console.Write("\r>");
-            Console.CursorVisible = false;
-            int position = 1;
-            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            int position = Cursor.MakeChoice(maxItems);
 
-            do
-            {
-                while (Console.KeyAvailable == false)
-                    Thread.Sleep(50); // Loop until input is entered.
-
-                key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.UpArrow && position > 1)
-                {
-                    Console.Write("\r ");
-                    Console.SetCursorPosition(0, Console.CursorTop - 1);
-                    //SFX.Play("Selection");
-                    position--;
-                    Console.Write("\r>");
-                }
-                else if (key.Key == ConsoleKey.DownArrow && position < (maxItems))
-                {
-                    Console.Write("\r ");
-                    Console.SetCursorPosition(0, Console.CursorTop + 1);
-                    //SFX.Play("Selection");
-                    position++;
-                    Console.Write("\r>");
-                }
-            } while (key.Key != ConsoleKey.Enter);
-            Console.CursorVisible = true;
-            maxItems = position;
-            Console.Clear();
             if (position == 1)
             {
                 Console.WriteLine("Dave screamed out into the void.");
@@ -59,60 +63,34 @@ namespace DaveMatthewsTextAdventure
             else if (position == 2)
             {
                 Player.dave.GetInventory().List();
-                System.Console.WriteLine("[Press the damn key, Shinji]");
-                System.ConsoleKeyInfo keyPress = System.Console.ReadKey();
-                MenuBuilder();
+                System.Console.WriteLine(" Back");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
+
+                Cursor.MakeChoice(maxItems);
+                //UseItem();
+                //MenuBuilder();
             }
             else if (position == 3)
             {
                 Console.WriteLine("Where would you like to go today?");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
                 Player.dave.PrintLocationList();
                 Console.WriteLine(" Back");
-                maxItems = Player.dave.GetLocationList().Count + 1;
-                Console.SetCursorPosition(0, Console.CursorTop - (maxItems));
-                Console.Write("\r>");
-                Console.CursorVisible = false;
-                position = 1;
-                ConsoleKeyInfo key2 = new ConsoleKeyInfo();
+                //An extra line is needed for this to work
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
 
-                do
-                {
-                    while (Console.KeyAvailable == false)
-                        Thread.Sleep(50); // Loop until input is entered.
-
-                    key2 = Console.ReadKey(true);
-                    if (key2.Key == ConsoleKey.UpArrow && position > 1)
-                    {
-                        Console.Write("\r ");
-                        Console.SetCursorPosition(0, Console.CursorTop - 1);
-                        //SFX.Play("Selection");
-                        position--;
-                        Console.Write("\r>");
-                    }
-                    else if (key2.Key == ConsoleKey.DownArrow && position < (maxItems))
-                    {
-                        Console.Write("\r ");
-                        Console.SetCursorPosition(0, Console.CursorTop + 1);
-                        //SFX.Play("Selection");
-                        position++;
-                        Console.Write("\r>");
-                    }
-                } while (key2.Key != ConsoleKey.Enter);
-                Console.CursorVisible = true;
-                maxItems = position;
-                Console.Clear();
-                // this probably needs to be worked so we can use the menu in other areas
-                if (position == 1)
-                {
-                    Console.WriteLine("Dave opened his front door and caught a big juicy whiff of fresh air. As he was about to make his trek to his new lambo that he couldn't fit in his garage due to his extensive knowledge collection, an increasingly closer figure began invading his personal space. As he approached, Dave thought he recognized him as a portly version of his flautist he recently fired, complete with red Dave Matthews Band cap from the 2008 Northern Thailand tour. However, at second glance, he couldn't tell. He got his pepper spray can at the ready.");
-                    System.Console.WriteLine("[Press the damn key, Shinji]");
-                    System.ConsoleKeyInfo keyPress = System.Console.ReadKey();
-                }
-                if (position == 2)
-                {
-                    MenuBuilder();
-                }
+                int choiceMade = Cursor.MakeChoice(maxItems + 1);
+                Location selectedLocation = Player.dave.GetLocationList()[choiceMade - 1];
+                GoToLocation(selectedLocation);
             }
         }
+
+        public static void RunHomeStory()
+        {
+            Console.WriteLine("Dave opened his front door and caught a big juicy whiff of fresh air. As he was about to make his trek to his new lambo that he couldn't fit in his garage due to his extensive knowledge collection, an increasingly closer figure began invading his personal space. As he approached, Dave thought he recognized him as a portly version of his flautist he recently fired, complete with red Dave Matthews Band cap from the 2008 Northern Thailand tour. However, at second glance, he couldn't tell. He got his pepper spray can at the ready.");
+            System.Console.WriteLine("[Press the damn key, Shinji]");
+            System.ConsoleKeyInfo keyPress = System.Console.ReadKey();
+        }
+
     }
 }
